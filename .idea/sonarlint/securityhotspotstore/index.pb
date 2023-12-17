17a3e0137bd5c4b1c2e4ab7b7559b27a45400f0a
anton.gorshkov@predori.com <anton.gorshkov@predori.com>
2023-12-17 08:45:41 +0100

    Your commit message

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
+<
+processor.py,7\8\78aa9bf4fc0926bb35d12d45a33b127f37fdd752
+_
+/venv/Lib/site-packages/pynput/_util/__init__.py,0\a\0a995e8f5041a4627ee9254348f8e8f02f4c3dfb
+b
+2venv/Lib/site-packages/pynput/keyboard/__init__.py,e\9\e9a4a979cf695c1353f12ade5b1b895b4a8ed7d7
+\
+,venv/Lib/site-packages/pyperclip/__init__.py,6\b\6bb3629c0972b44ced257976b6ea1279fa9593c8
---
+++ processor.py
@@ -0,0 +1,331 @@
+# # import string
+# # import pyperclip
+# # from pynput import keyboard
+# # from pynput.keyboard import Key
+# #
+# # # Initialize an empty string current_word outside the on_press function
+# # current_word = ""
+#
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         # Check if the pressed key is alphanumeric
+# #         if key.char.isalnum():
+# #             current_word += key.char
+# #         elif key == Key.space or key.char in string.punctuation:
+# #             # Copy current_word to the clipboard and reset it
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #     except AttributeError:
+# #         pass  # special key pressed
+# #
+# #
+#
+#
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         # Check if the pressed key is alphanumeric
+# #         if key.char.isalnum():
+# #             current_word += key.char
+# #         # Check if the pressed key is a space or a punctuation mark
+# #         elif key == Key.space or (hasattr(key, 'char') and key.char in string.punctuation):
+# #             # Copy current_word to the clipboard and reset it
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #     except AttributeError:
+# #         pass  # special key pressed
+# #
+# #
+#
+#
+# import string
+# import pyperclip
+# from pynput import keyboard
+# from pynput.keyboard import Key
+#
+# EMPTY_STR = ""
+#
+# V_CHAR = 'v'
+#
+# LAST_CHAR_INDEX = -1
+#
+# V = 'v'
+#
+# # Initialize an empty string current_word outside the on_press function
+# # This variable will be used to store the characters of a word as they are typed
+# current_word = ""
+#
+# # def on_press(key):
+# #     """
+# #     This function is called whenever a key is pressed. It checks if the pressed key is alphanumeric, a space, or a punctuation mark.
+# #     If the key is alphanumeric, it is added to the current_word string.
+# #     If the key is a space or a punctuation mark, the current_word is copied to the clipboard and then reset to an empty string.
+# #     If the key is a special key (like Shift, Ctrl, etc.), an AttributeError is raised and caught, and the function does nothing.
+# #
+# #     Args:
+# #         key (Key): The key that was pressed.
+# #     """
+# #     global current_word
+# #     try:
+# #         # Check if the pressed key is alphanumeric
+# #         #
+# #         # if key.char.isalnum():  # TODO no attribute 'char' error
+# #         if hasattr(key, 'char') and key.char.isalnum():
+# #             # TODO: what if the key is a space or a punctuation mark?
+# #             # Add the pressed key to the current_word string
+# #             current_word += key.char  # TODO no attribute 'char' error
+# #         # Check if the pressed key is a space or a punctuation mark
+# #         elif key == Key.space or (
+# #                 hasattr(key, 'char') and key.char in string.punctuation):  # TODO no attribute 'char' error
+# #             # Copy current_word to the clipboard and reset it
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #     except AttributeError:
+# #         raise AttributeError  # special key pressed
+#
+#
+# import string
+# import pyperclip
+# from pynput import keyboard
+# from pynput.keyboard import Key
+#
+# current_word = ""
+#
+# # TODO the start and the finish of the sentence is not in the clipboard. Fix it.
+# # TODO the deleted characters should not be in the clipboard. Fix it.
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key == Key.space or (isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #     except AttributeError:
+# #         pass
+# # todo write the program which save every typed word in the clipboard automatically
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key in [Key.space, Key.enter, Key.shift_r] or (isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #     except AttributeError:
+# #         pass
+#
+# # todo find the way on how to copy the last word in the clipboard
+# # TODO the deleted characters should not be in the clipboard. Fix it.
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key in [Key.space, Key.enter, Key.shift_r] or (
+# #                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #         elif key == Key.backspace:
+# #             current_word = current_word[:-1]
+# #     except AttributeError:
+# #         pass
+#
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key in [Key.space, Key.enter, Key.shift_r] or (
+# #                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #         elif key == Key.backspace:
+# #             current_word = current_word[:-1]  # Remove the last character from current_word
+# #             pyperclip.copy(current_word)  # Update the clipboard with the updated current_word
+# #     except AttributeError:
+# #         pass
+#
+#
+# # def on_press(key):
+# #     global current_word
+# #     try:
+# #         # Check if win-v is pressed
+# #         if key == keyboard.Key.cmd and keyboard.KeyCode.from_char(V):
+# #             return  # todo not sure about return, maybe continue
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key in [Key.space, Key.enter, Key.shift_r] or (
+# #                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = ""
+# #         elif key == Key.backspace:
+# #             current_word = current_word[:LAST_CHAR_INDEX]  # Remove the last character from current_word
+# #             pyperclip.copy(current_word)  # Update the clipboard with the updated current_word
+# #     except AttributeError:
+# #         pass
+# #
+# #
+# # # Start the keylogger
+# # with keyboard.Listener(on_press=on_press) as listener:
+# #     listener.join()
+#
+# ####################################################################################################
+# # Start the keylogger
+# # with keyboard.Listener(on_press=on_press) as listener:
+# #     listener.join()
+#
+# # Start the keylogger
+# # The Listener object listens for key presses and calls the on_press function whenever a key is pressed
+# # with keyboard.Listener(on_press=on_press) as listener:
+# #     listener.join()
+# ####################################################################################################
+#
+# # from pynput import keyboard
+# # from pynput.keyboard import Key
+# #
+# # current_word = ""
+# # win_pressed = False
+# #
+# #
+# # def on_press(key):
+# #     global current_word, win_pressed
+# #     try:
+# #         if key == keyboard.Key.cmd:
+# #             win_pressed = True
+# #             return
+# #         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+# #             return
+# #         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+# #             current_word += key.char
+# #         elif key in [Key.space, Key.enter, Key.shift_r] or (
+# #                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+# #             pyperclip.copy(current_word)
+# #             current_word = EMPTY_STR
+# #         elif key == Key.backspace:
+# #             current_word = current_word[:LAST_CHAR_INDEX]
+# #             pyperclip.copy(current_word)
+# #     except AttributeError:
+# #         pass
+# #
+# #
+# # def on_release(key):
+# #     global win_pressed
+# #     if key == keyboard.Key.cmd:
+# #         win_pressed = False
+# #
+# #
+# # with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+# #     listener.join()
+import string
+
+import pyperclip
+from pynput import keyboard
+from pynput.keyboard import Key
+
+EMPTY_STR = ""
+V_CHAR = 'v'
+LAST_CHAR_INDEX = -1
+
+current_word = EMPTY_STR
+win_pressed = False
+
+
+#
+#
+# def on_press(key):
+#     global current_word, win_pressed
+#     try:
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         if isinstance(key, keyboard.KeyCode) and key.char.isalnum():
+#             current_word += key.char
+#         elif key in [Key.space, Key.enter, Key.shift_r] or (
+#                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation):
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass
+#
+#
+
+
+# def on_press(key):
+#     global current_word, win_pressed
+#     try:
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         if isinstance(key, keyboard.KeyCode) and (key.char.isalnum() or key.char == '#'):
+#             current_word += key.char
+#         elif key in [Key.space, Key.enter, Key.shift_r] or (
+#                 isinstance(key, keyboard.KeyCode) and key.char in string.punctuation and key.char != '#'):
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass  # todo process special keys
+
+
+# def on_press(key):
+#     global current_word, win_pressed
+#     try:
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         if isinstance(key, keyboard.KeyCode):
+#             current_word += key.char
+#         elif key in [Key.space, Key.enter, Key.shift_r] or (
+#                 isinstance(key, keyboard.KeyCode) and key.char != '#'):
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass
+
+
+def on_press(key):
+    global current_word, win_pressed
+    try:
+        if key == keyboard.Key.cmd:
+            win_pressed = True
+            return
+        if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+            return
+        if isinstance(key, keyboard.KeyCode):
+            current_word += key.char
+        elif key in [Key.space, Key.enter, Key.shift_r]:
+            pyperclip.copy(current_word)
+            current_word = EMPTY_STR
+        elif key == Key.backspace:
+            current_word = current_word[:LAST_CHAR_INDEX]
+            pyperclip.copy(current_word)
+    except AttributeError:
+        pass
+
+
+def on_release(key):
+    global win_pressed
+    if key == keyboard.Key.cmd:
+        win_pressed = False
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
