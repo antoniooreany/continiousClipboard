8cb76d9e061eb249b9571a2ebd70d5cf38076390
anton.gorshkov@predori.com <anton.gorshkov@predori.com>
2023-12-17 13:40:22 +0100

    Fix everything except of the redundant symbols which are put into clipboard like independent word

--- processor.py
+++ processor.py
@@ -221,16 +221,17 @@
 import string
 
 import pyperclip
-from pynput import keyboard
-from pynput.keyboard import Key
-
-EMPTY_STR = ""
-V_CHAR = 'v'
-LAST_CHAR_INDEX = -1
-
-current_word = EMPTY_STR
-win_pressed = False
-
+# import pyperclip
+# from pynput import keyboard
+# from pynput.keyboard import Key
+#
+# EMPTY_STR = ""
+# V_CHAR = 'v'
+# LAST_CHAR_INDEX = -1
+#
+# current_word = EMPTY_STR
+# win_pressed = False
+#
 
 #
 #
@@ -299,8 +300,120 @@ win_pressed = False
 #         pass
 
 
+# def on_press(key):
+#     global current_word, win_pressed
+#     try:
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         if isinstance(key, keyboard.KeyCode):
+#             current_word += key.char
+#         elif key in [Key.space, Key.enter, Key.shift_r]:
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass
+#
+#
+# def on_release(key):
+#     global win_pressed
+#     if key == keyboard.Key.cmd:
+#         win_pressed = False
+#
+#
+# with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
+
+# from pynput import keyboard
+# from pynput.keyboard import Key
+#
+# # Constants for the program
+# EMPTY_STR = ""
+# V_CHAR = 'v'
+# LAST_CHAR_INDEX = -1
+#
+# # Global variables to hold the current word and the state of the 'win' key
+# current_word = EMPTY_STR
+# win_pressed = False
+#
+#
+# def on_press(key):
+#     """
+#     Function to handle key press events.
+#
+#     Args:
+#         key (pynput.keyboard.Key): The key that was pressed.
+#
+#     Global:
+#         current_word (str): The current word being typed.
+#         win_pressed (bool): Whether the 'win' key is pressed.
+#     """
+#     global current_word, win_pressed
+#     try:
+#         # If 'win' key is pressed, set win_pressed to True and return
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         # If 'win' key is pressed and 'v' is pressed, return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         # If any character key is pressed, add it to current_word
+#         if isinstance(key, keyboard.KeyCode):
+#             current_word += key.char
+#         # If space, enter, or shift_r is pressed, or a character key other than '#' is pressed,
+#         # copy current_word to clipboard and reset current_word
+#         elif key in [Key.space, Key.enter, Key.shift_r]:
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         # If backspace is pressed, remove the last character from current_word and update the clipboard
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass
+#
+#
+# def on_release(key):
+#     """
+#     Function to handle key release events.
+#
+#     Args:
+#         key (pynput.keyboard.Key): The key that was released.
+#
+#     Global:
+#         win_pressed (bool): Whether the 'win' key is pressed.
+#     """
+#     global win_pressed
+#     # If 'win' key is released, set win_pressed to False
+#     if key == keyboard.Key.cmd:
+#         win_pressed = False
+#
+#
+# # Start the listener to listen for key press and release events
+# with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
+
+# todo /fix  when I write 'bbbbXRTH' and then remove 'XRTH' the app should add only 'bbbb'. rewrite like this
+
+from pynput import keyboard
+from pynput.keyboard import Key
+
+EMPTY_STR = ""
+V_CHAR = 'v'
+LAST_CHAR_INDEX = -1
+
+current_word = EMPTY_STR
+win_pressed = False
+backspace_pressed = False
+
+
 def on_press(key):
-    global current_word, win_pressed
+    global current_word, win_pressed, backspace_pressed
     try:
         if key == keyboard.Key.cmd:
             win_pressed = True
@@ -308,15 +421,38 @@ def on_press(key):
         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
             return
         if isinstance(key, keyboard.KeyCode):
+            if backspace_pressed:
+                backspace_pressed = False
+                pyperclip.copy(current_word)
             current_word += key.char
         elif key in [Key.space, Key.enter, Key.shift_r]:
             pyperclip.copy(current_word)
             current_word = EMPTY_STR
         elif key == Key.backspace:
             current_word = current_word[:LAST_CHAR_INDEX]
-            pyperclip.copy(current_word)
+            backspace_pressed = True
     except AttributeError:
         pass
+
+
+# def on_press(key):
+#     global current_word, win_pressed
+#     try:
+#         if key == keyboard.Key.cmd:
+#             win_pressed = True
+#             return
+#         if win_pressed and keyboard.KeyCode.from_char(V_CHAR):
+#             return
+#         if isinstance(key, keyboard.KeyCode) and key.char is not None:
+#             current_word += key.char
+#         elif key in [Key.space, Key.enter, Key.shift_r]:
+#             pyperclip.copy(current_word)
+#             current_word = EMPTY_STR
+#         elif key == Key.backspace:
+#             current_word = current_word[:LAST_CHAR_INDEX]
+#             pyperclip.copy(current_word)
+#     except AttributeError:
+#         pass
 
 
 def on_release(key):
@@ -327,5 +463,3 @@ def on_release(key):
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
-
